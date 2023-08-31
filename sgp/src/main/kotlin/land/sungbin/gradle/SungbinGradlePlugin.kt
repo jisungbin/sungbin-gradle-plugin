b2d7a2b8481c347199e590a5ab9ed1b9a3c28f72
jisungbin <ji@sungb.in>
2023-08-31 18:02:01 +0900

    wip

---
+++ sgp/src/main/kotlin/land/sungbin/gradle/SungbinGradlePlugin.kt
@@ -0,0 +1,17 @@
+/*
+ * Developed by Ji Sungbin 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/jisungbin/sungbin-gradle-plugin/blob/main/LICENSE
+ */
+
+package land.sungbin.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class SungbinGradlePlugin : Plugin<Project> {
+  override fun apply(target: Project) {
+    TODO("Not yet implemented")
+  }
+}
