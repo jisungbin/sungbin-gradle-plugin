e7645dbdade6381d4ed4c966950846e371941514
jisungbin <ji@sungb.in>
2023-09-01 15:57:24 +0900

    Remove android dependency from sample module

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,5 +1,3 @@
-import land.sungbin.gradle.util.jdkVersion
-
 /*
  * Developed by Ji Sungbin 2023.
  *
@@ -8,19 +6,11 @@ import land.sungbin.gradle.util.jdkVersion
  */
 
 plugins {
-  id("com.android.library")
+  kotlin("jvm")
   id("land.sungbin.gradle") version "0.1.0"
 }
 
-android {
-  namespace = "sungbin.gradle.sample.helloworld"
-  jdkVersion(17)
-}
-
 sgp {
-  android {
-    kotlinSourceSet()
-  }
   features {
     ktor {
       client()
